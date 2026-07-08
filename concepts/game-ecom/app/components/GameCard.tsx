e1646a0aa1b1bf5fe89989aa7a85ec1b7ef1547a
jphani787 <jphanindra787@gmail.com>
2026-07-09 00:03:29 +0530

    added login page

--- concepts/game-ecom/app/components/GameCard.tsx
+++ concepts/game-ecom/app/components/GameCard.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-
-const GameCard = () => {
-  return <div>GameCard</div>;
+type GameProps = {
+  game: {
+    id: number;
+    name: string;
+    img: string;
+  };
+};
+const GameCard = ({ game }: GameProps) => {
+  return (
+    <div className="bg-gray-800  text-white p-4 rounded-lg shadow-md">
+      <img
+        src={game.img}
+        alt={game.name}
+        className="w-full h-32 object-cover rounded-md"
+      />
+      <h3 className="mt-2 text-lg font-semibold">{game.name}</h3>
+    </div>
+  );
 };
 
 export default GameCard;
--- concepts/game-ecom/app/games/page.tsx
+++ concepts/game-ecom/app/games/page.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import { games } from "@/db/data";
+import GameCard from "../components/GameCard";
 
 const Games = () => {
   return (
-    <div>
+    <div className="flex h-screen">
       <Sidebar />
       <div className="flex-1 bg-gray-100 p-5">
         <h1 className="text-2xl font-bold md-4">Featured Games</h1>
-        <div className="flex gap-4"></div>
+        <div className="flex gap-4">
+          {games.map((game) => (
+            <GameCard key={game.id} game={game} />
+          ))}
+        </div>
       </div>
     </div>
   );
